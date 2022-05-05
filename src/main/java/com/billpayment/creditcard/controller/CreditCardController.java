2b33cbe5c1ea63bf4769fb0fc162774c8304c351
Jagadeesh <jaggu5733@gmail.com>
2022-05-05 23:46:15 +0530

    Created a endpoint for register and login saved the data in database successfully.

--- src/main/java/com/billpayment/creditcard/controller/CreditCardController.java
+++ src/main/java/com/billpayment/creditcard/controller/CreditCardController.java
@@ -2,10 +2,12 @@ package com.billpayment.creditcard.controller;
 
 import com.billpayment.creditcard.dto.BaseResponse;
 import com.billpayment.creditcard.dto.RegisterDetailRequest;
+import com.billpayment.creditcard.dto.UserDetailRequest;
 import com.billpayment.creditcard.service.CreditCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,19 +19,42 @@ public class CreditCardController {
     private CreditCardService creditCardService;
 
 
+    /*
+     * post mapping for register user detail
+     *
+     * */
+
+
     @PostMapping("register-user-detail")
-    public ResponseEntity<BaseResponse> registerUserDetail(@RequestBody RegisterDetailRequest registerDetailRequest)
-    {
-        if (registerDetailRequest==null || registerDetailRequest.getRegisterMobileNo().isEmpty())
-        {
-            BaseResponse baseResponse= new BaseResponse();
+    public ResponseEntity<BaseResponse> registerUserDetail(@RequestBody UserDetailRequest userDetailRequest) {
+        if(userDetailRequest == null || userDetailRequest.getMobileNo().isEmpty()) {
+            BaseResponse baseResponse = new BaseResponse();
             baseResponse.setMessage("registration detail can not be empty");
+            baseResponse.setHttpStatus(HttpStatus.BAD_REQUEST);
+            baseResponse.setHttpStatusCode(HttpStatus.BAD_REQUEST.value());
+
+            return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
+        }
+        return creditCardService.userDetailRequest(userDetailRequest);
+    }
+
+    /*
+     * post mapping for login user
+     *
+     * */
+
+    @PostMapping("user-login")
+    public ResponseEntity<BaseResponse> loginUser(  @RequestBody UserDetailRequest userDetailRequest)
+    {
+        if (userDetailRequest==null)
+        {
+            BaseResponse baseResponse = new BaseResponse();
+            baseResponse.setMessage("please enter valid credentials");
             baseResponse.setHttpStatus(HttpStatus.BAD_REQUEST);
             baseResponse.setHttpStatusCode(HttpStatus.BAD_REQUEST.value());
 
             return new ResponseEntity<>(baseResponse,HttpStatus.BAD_REQUEST);
         }
-        return creditCardService.registerUserDetail(registerDetailRequest);
+        return creditCardService.loginUser(userDetailRequest);
     }
-
 }
--- src/main/java/com/billpayment/creditcard/service/CreditCardService.java
+++ src/main/java/com/billpayment/creditcard/service/CreditCardService.java
@@ -3,10 +3,13 @@ package com.billpayment.creditcard.service;
 
 import com.billpayment.creditcard.dto.BaseResponse;
 import com.billpayment.creditcard.dto.RegisterDetailRequest;
+import com.billpayment.creditcard.dto.UserDetailRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface CreditCardService {
-    ResponseEntity<BaseResponse> registerUserDetail(RegisterDetailRequest registerDetailRequest);
+    ResponseEntity<BaseResponse> userDetailRequest(UserDetailRequest userDetailRequest);
+
+    ResponseEntity<BaseResponse> loginUser( UserDetailRequest userDetailRequest);//
 }
